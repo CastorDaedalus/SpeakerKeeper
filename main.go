7588648fdf5b8b5f777aa7a86ca9551ed18f5ce1
Joshua Vannatter <joshnotes2u@gmail.com>
2023-03-31 22:42:30 -0400

    Rearranging some stuff

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-
 	"github.com/gordonklaus/portaudio"
 )
 
@@ -23,48 +18,4 @@ func main() {
 
 	initMainLoop()
 
-}
-
-func loadConfigData() Config {
-
-	// Check if the file exists
-	_, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		println("Creating config file")
-		configFile, err := os.Create(filename)
-		ChkErr(err)
-		defer configFile.Close()
-		return baseConfig
-	}
-
-	configFile, err := os.Open(filename)
-	ChkErr(err)
-	defer configFile.Close()
-
-	err = json.NewDecoder(configFile).Decode(&runtimeConfig)
-
-	if err == io.EOF {
-		println("Generating base data")
-		saveConfigData(runtimeConfig)
-		return runtimeConfig
-	}
-	ChkErr(err)
-
-	fmt.Printf("Loaded config file: %s\n", filename)
-
-	return runtimeConfig
-
-}
-
-func saveConfigData(config Config) {
-	configFile, err := os.Create(filename)
-	ChkErr(err)
-	defer configFile.Close()
-
-	encoder := json.NewEncoder(configFile)
-	encoder.SetIndent("", "\t")
-	err = encoder.Encode(config)
-	ChkErr(err)
-
-	println("Save complete")
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
 func ChkErrMsg(err error, msg string) {
 	if err != nil {
@@ -20,4 +25,48 @@ func If[T any](cond bool, vtrue, vfalse T) T {
 		return vtrue
 	}
 	return vfalse
+}
+
+func loadConfigData() Config {
+
+	// Check if the file exists
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		println("Creating config file")
+		configFile, err := os.Create(filename)
+		ChkErr(err)
+		defer configFile.Close()
+		return baseConfig
+	}
+
+	configFile, err := os.Open(filename)
+	ChkErr(err)
+	defer configFile.Close()
+
+	err = json.NewDecoder(configFile).Decode(&runtimeConfig)
+
+	if err == io.EOF {
+		println("Generating base data")
+		saveConfigData(runtimeConfig)
+		return runtimeConfig
+	}
+	ChkErr(err)
+
+	fmt.Printf("Loaded config file: %s\n", filename)
+
+	return runtimeConfig
+
+}
+
+func saveConfigData(config Config) {
+	configFile, err := os.Create(filename)
+	ChkErr(err)
+	defer configFile.Close()
+
+	encoder := json.NewEncoder(configFile)
+	encoder.SetIndent("", "\t")
+	err = encoder.Encode(config)
+	ChkErr(err)
+
+	println("Save complete")
 }
