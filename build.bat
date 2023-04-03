8ca5b2512bb5699c7e0b17a237552223c17d7b97
Joshua Vannatter <joshnotes2u@gmail.com>
2023-04-03 13:49:54 -0400

    Change build file

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
 go build -ldflags "-H=windowsgui" -o SpeakerKeeper_windows.exe
+
+setlocal
+
+set FILENAMES=SpeakerKeeper_windows.exe,sound.mp3
+set ZIPNAME=SpeakerKeeper_windows.zip
+
+powershell -Command "Compress-Archive -Path %FILENAMES% -DestinationPath %ZIPNAME%"
+
+endlocal
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.20
 require (
 	github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc
 	github.com/ncruces/zenity v0.10.6
-	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 )
 
 require (
